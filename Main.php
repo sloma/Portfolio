353df17cf2e2b36bdcea540306a2262beb622043
salim bouassida <sloma27@hotmail.com>
2015-02-09 11:37:14 -0800

    feb 9th portofolio update

---
+++ Main.php
@@ -0,0 +1,265 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Salim Bouassida</title>
+
+    <link rel="stylesheet" href="css/foundation.css" />
+    <link rel="stylesheet" href="css/custom.css" />
+    <link rel="stylesheet" href="foundation-icons/foundation-icons.css" />
+    <script src="js/vendor/modernizr.js"></script>
+
+
+</head>
+
+<body>
+<div id="top"></div>
+<div class="contain-to-grid fixed">
+    <nav class="top-bar" data-topbar>
+        <ul class="title-area">
+
+            <!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->
+            <li class="toggle-topbar menu-icon">
+                <a href="#">
+                    <span></span>
+                </a>
+            </li>
+        </ul>
+        <section class="top-bar-section">
+            <!-- Right Nav Section -->
+            <ul id="nav" class="right">
+
+                <li>
+
+                    <a href="#top">Salim Bouassida</a>
+                </li>
+
+                <li>
+                    <a href="#Education">Education</a>
+                </li>
+                <li>
+                    <a href="#Workexperience">Work experience</a>
+                </li>
+                <li>
+                    <a href="#Projects">Projects</a>
+                </li>
+                <li>
+                    <a href="#contact">Contact</a>
+                </li>
+                <li class="social" style="font-size:larger"> <a href="http://facebook.com/salim.bouassida.7"><i class="fi-social-facebook"></i></a></li>
+                <li class="social" style="font-size:larger"> <a href="https://www.linkedin.com/profile/view?id=186525460"><i class="fi-social-linkedin"></i></a></li>
+                <li class="social" style="font-size:larger"> <a href="https://github.com/sloma"><i class="fi-social-github"></i></a></li>
+            </ul>
+
+        </section>
+    </nav>
+</div>
+
+<section id="me" style="margin-top: 9%; margin-left:10%">
+    <div class="row" style=" max-width: none; margin-left: 0%">
+
+
+            <img src="img/profile.jpg"style="max-width: 15%; float:left; margin-left: 5%" >
+
+        <div class="small-7 column" style="float:left">
+           <p>I am looking for an Entry level position for June 2015 in one of the following fields:<br>
+<br>- Game development
+<br>- Project management
+<br>- Web development
+<br>- Quality assurance
+<br>- Software development<br>
+
+<br>I graduated from a French high school, but my love for travel and my desire to expand my horizons pushed me to do my post-secondary education in Canada. 
+Having a passion for new technologies, it was only natural for me to choose computer science as a career path. Being a problem solver, building things, testing their limits and being ambitious coincide with my love for computer science. 
+As an individual who always strives for improvement, I am constantly seeking opportunities to better myself personally and through my work. I like to set goals for myself  and find ways to achieve them. I am constantly eager to learn new aspects of computer science. 
+Furthermore, I am confident that with my past experiences and my current skill set, I will be able to learn and adapt quickly to any new role.
+</p>
+        </div>
+
+
+    </div>
+</section>
+
+
+<section id="Education" style="background: white;; margin-bottom: 3%;">
+    <div class="row" style="background: #003f54; max-width: none">
+
+        <h1 style="margin-left: 5%"> Education</h1>
+
+    </div>
+    <div class="row" style=" margin-top: 10%">
+
+
+        <div class="large-8 columns">
+
+            <h2>B.sc of Computer Science </h2>
+            <h4>University of British Columbia Okanagan   2010-2015 </h4>
+            <br>
+            <p>Undergraduate coursework: Software Engineering, Project Management, Database, Algorithms,
+                Programming Languages, Computer Architecture, Linear Programming, Numerical Optimization
+                Practical Game Programming
+            </p>
+        </div>
+        <div class="large-4 columns">
+            <img src="img/UBC.jpg" style="width: 80%">
+        </div>
+    </div>
+</section>
+<section id="Workexperience" style="background: white; margin-top: 2%;">
+    <div class="row" style="background: #003f54; max-width: none">
+
+        <h1 style="margin-left: 5%"> Work experience</h1>
+
+    </div>
+    <div class="row" style=" margin-top: 5%">
+
+
+        <div class="large-8 columns">
+
+            <h2>Scrum master/Developer</h2>
+            <h4>GreenStep</h4>
+            <br>
+            <p>•	Upgraded the “Ecobase” software that provides businesses with carbon management and audit reports.
+          <br>      •	Team of 5 members using the Agile methodology (Sprint planning, Scrums, Acceptance criteria).
+                <br>     •	Improved loading time by 3s by trasnfering the flash software to HTML5
+                <br>     •	Remodeled some of the software and added new features as requested
+           <br>     •	Html, CSS, PHP, JavaScript, SQL, Ajax.
+            </p>
+        </div>
+        <div class="large-4 columns">
+            <img src="img/ecobase.png" style="width: 50%; margin-left: 10%">
+        </div>
+    </div>
+
+    <div class="row">
+        <hr>
+        <div class="large-8 columns">
+
+            <h2>Kitchen Staff</h2>
+            <h4>Well Pub- UBC Student Union Okanagan</h4>
+            <br>
+            <p> •	Provided excellent customer service and communication with customers and coworkers
+              <br>  •	Worked efficiently under high-pressure environment during rush hours
+       <br>         •	Adaptable in any situation with a team and individually
+
+            </p>
+        </div>
+        <div class="large-4 columns">
+            <img src="img/UBCSUO.jpg" style="width: 63%;margin-left: 4%">
+        </div>
+        </div>
+
+    <div class="row">
+        <hr>
+        <div class="large-8 columns">
+
+            <h2>Student Host</h2>
+            <h4>UBC International Program and Services</h4>
+            <br>
+            <p>•	Planned and managed a variety of University events
+             <br>   •	Networked with students
+
+            </p>
+        </div>
+        <div class="large-4 columns">
+            <img src="img/UBC.jpg" style="width:42%; margin-left: 15% ">
+        </div>
+    </div>
+</section>
+<section id="Projects"style="background: white; margin-bottom: 0%;">
+    <div class="row" style="background: #003f54; max-width: none">
+
+        <h1 style="margin-left: 5%"> Projects</h1>
+
+    </div>
+    <div class="row" style=" margin-top: 5%">
+
+
+        <div class="large-8 columns">
+
+            <h2>Scrum master/Developer</h2>
+            <h4>Rent Me :Sept 2014- Dec 2015</h4>
+            <br>
+            <p> •	Developed a website that enables users to rate roommates, landlord, and properties.
+             <br>   •	Html, CSS, PHP, Javascript, SQL, Ajax.
+            </p>
+        </div>
+        <div class="large-4 columns">
+            <img src="img/HTML.png" style="width: 50%; margin-left: 10%">
+        </div>
+    </div>
+
+    <div class="row">
+        <hr>
+        <div class="large-8 columns">
+
+            <h2>Developer</h2>
+            <h4>My trivia: Jan 2015- Present</h4>
+            <br>
+            <p> •	Programmed a PC game that helps students review for their exams.
+                <br>   •	C++, Allegro library
+            </p>
+        </div>
+        <div class="large-4 columns">
+            <img src="img/C++.png" style="width: 63%;margin-left: 4%">
+        </div>
+    </div>
+
+    <div class="row">
+        <hr>
+        <div class="large-8 columns">
+
+            <h2>Developer</h2>
+            <h4>Chatbot: Sept 2013-Dec 2014</h4>
+            <br>
+            <p> text about work work work work text about work work work work text about work work work work text about work work work work
+            </p>
+        </div>
+        <div class="large-4 columns">
+            <img src="img/java.png" style="width:55%; margin-left: 15% ">
+        </div>
+    </div>
+</section>
+<section  id="contact">
+    <div class="row" >
+        <div class="large-5 columns" style="color: transparent ; margin-top:3%">
+
+            <h4>Name : Salim Bouassida</h4>
+            <h4>Email : salim.bouassida@gmail.com</h4>
+            <h4>Phone: (250) 863-1842 </h4>
+             <a href="http://facebook.com/salim.bouassida.7"><i class="fi-social-facebook" style="font-size: xx-large; color: #003f54"></i></a>
+             <a href="https://www.linkedin.com/profile/view?id=186525460"><i class="fi-social-linkedin" style="font-size: xx-large; color: #003f54"></i></a>
+            <a href="https://github.com/sloma"><i class="fi-social-github" style="font-size: xx-large; color: #003f54"></i></a>
+
+        </div>
+
+        </div>
+    </section>
+
+
+<script src="js/vendor/jquery.js"></script>
+<script src="js/foundation.min.js"></script>
+<script>
+    $(document).foundation();
+</script>
+<script>
+    $(function() {
+        $('a[href*=#]:not([href=#])').click(function() {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
+</script>
+</body>
+
+</html>
